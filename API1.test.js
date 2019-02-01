5a2b10d43f08cd371533707b56d80528ff42d488
Pulkit Agarwal <pulkit_agarwal-bng@external.mckinsey.com>
2019-02-01 12:27:57 +0530

    improvement over test cases

--- API1.test.js
+++ API1.test.js
@@ -1,29 +1,32 @@
+const https = require('https');
 const functions = require('./API1.js');
 
 describe('promise', () => {
-  const promises = [];
+  // const promises = [];
+  // const accumulator = {};
   it('should have two properties', (done) => {
-    const callback = (response) => {
-      let result = '';
-      response.setEncoding('utf8').on('data', (data) => {
-        result += data;
-      });
-      response.on('end', async () => {
-        const books = JSON.parse(result).books;
-        books.forEach((book) => {
-          promises.push(functions.combineArray(book));
+    const makeGetRequest = (accumulator, promises) => {
+      https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (response) => {
+        let result = '';
+        // const accumulator = {};
+        response.setEncoding('utf8').on('data', (data) => {
+          result += data;
         });
+        response.on('end', async () => {
+          const books = JSON.parse(result).books;
+          books.forEach((book) => {
+            promises.push(functions.combineArray(accumulator, book));
+          });
 
-        Promise.all(promises).then((data) => {
-          expect(data['Sidney Sheldon'].length).toEqual(7);
+          Promise.all(promises).then(() => {
+            expect(accumulator['J K Rowling'].length).toEqual(7);
+            expect(accumulator['Sidney Sheldon'].length).toEqual(5);
+          });
         });
-
-        done();
-        // .then(() => console.log(accumulator));
       });
     };
-    promise.then((data) => {
-      expect(data['J K Rowling'].length).toEqual(7);
-    });
+
+    done();
+    makeGetRequest({}, []);
   });
 });
